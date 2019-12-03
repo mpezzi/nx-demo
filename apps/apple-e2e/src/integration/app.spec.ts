6f889aa05b768931185230f2c1b68c2df30f4d65
Michael Pezzi <michael.pezzi@acquia.com>
2019-12-03 15:22:34 -0500

    adjusting apple app

--- apps/apple-e2e/src/integration/app.spec.ts
+++ apps/apple-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('apple', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to apple!');
+    getGreeting().contains('apple');
   });
 });
