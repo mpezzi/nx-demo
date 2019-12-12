0ef142bd50520b431150c33f72348e1fbf851e37
Michael Pezzi <michael.pezzi@acquia.com>
2019-12-12 16:45:09 -0500

    redirect / to /apple/

--- servers/dashboard/src/main.ts
+++ servers/dashboard/src/main.ts
@@ -11,6 +11,10 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to dashboard!' });
 });
 
+app.get('/', (req, res) => {
+  res.redirect('/apple/');
+});
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
